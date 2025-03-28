c3fd479f4a665ae8ed93e26d44eb959bea2d0daa
Aaron <aarondeo30@gmail.com>
2025-03-28 09:44:46 -0700

    preloaded sounds

--- local_dev/backend/game.py
+++ local_dev/backend/game.py
@@ -3,16 +3,18 @@ import random
 #from gpiozero import LED, Buzzer
 import keyboard
 import pygame
+from pynput import keyboard
 
-
+pygame.mixer.init(frequency=22050, size=-8, channels=2, buffer=512)
+game_start_countdown_sound = pygame.mixer.Sound('game_start_countdown.mp3')
+game_end_sound = pygame.mixer.Sound('game_end.mp3')
 
 #redLED = LED(17)
 #greenLED = LED(27)
 #buzzer = Buzzer(22)
 
-def play_sound(audio_file):
-    sound1 = pygame.mixer.Sound(audio_file)
-    sound1.play()
+def play_sound(sound):
+    sound.play()
     
 def get_player_names(num_players):
     players = {}
@@ -24,8 +26,14 @@ def get_player_names(num_players):
 
 def countdown(seconds=5):
     # Play game start countdown audio
-    play_sound('game_start_countdown.mp3')
+    play_sound(game_start_countdown_sound)
 
+def on_press(key):
+    global game_won
+    if key == keyboard.Key.space:
+        # Set flag and stop listener
+        game_won = True
+        return False  # Stop the listener
 
 def red_light_green_light_loop(duration, on_green, on_red, players):
     print("\n🔫 Game Start!")
@@ -33,18 +41,18 @@ def red_light_green_light_loop(duration, on_green, on_red, players):
     eliminated_players = set()  # Track eliminated players
 
     while time.time() - start_time < duration:
+        if game_won:
+            print("\n🏁 Game Won!")
+            play_sound(game_end_sound)
+            return
+
         if len(eliminated_players) == len(players):  # Check if all are eliminated
-            #buzzer.on()
-            time.sleep(0.1)
-            #buzzer.off()
-            #buzzer.on()
-            time.sleep(0.1)
-            #buzzer.off()
-            #buzzer.on()
-            time.sleep(0.5)
-            #buzzer.off()
+            if len(players) == 1:
+                print("\n🏆 Player {} wins!".format(list(players.keys())[0]))
+                play_sound(game_end_sound)
+                return
             print("\n🚨 All players eliminated! Game Over!")
-            play_sound('game_end.mp3')
+            play_sound(game_end_sound)
             return
         
         # GREEN LIGHT
@@ -73,4 +81,4 @@ def red_light_green_light_loop(duration, on_green, on_red, players):
         eliminated_players.update(on_red(players, red_duration))  # Update eliminated players
 
     print("\n🏁 Game Over!")
-    play_sound('game_end.mp3')
+    play_sound(game_end_sound)
